b63aa87f7e504611e76815b262a49bfc3cf43b00
Udi Oron <udioron@gmail.com>
2015-10-15 14:28:00 +0300

    Added static files

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 import os
+import pathlib
 
-from bottle import route, run
+from bottle import route, run, static_file
 
 PORT = int(os.environ.get("PORT", 5000))
+BASE_PATH = pathlib.Path(__file__).parent
 
 
 @route('/')
 def home():
-    return "Hello world!"
+    return 'Hello world! <img src="static/python.png"/>'
+
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root=str(BASE_PATH / 'static'))
 
 
 run(host='0.0.0.0', port=PORT)
